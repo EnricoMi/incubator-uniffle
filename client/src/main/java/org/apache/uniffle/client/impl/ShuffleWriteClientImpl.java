a2806a15d54cc2b5f398997fc57e76abb11630cd
Enrico Minack <github@enrico.minack.dev>
2024-02-02 15:57:53 +0100

    Fix committing shuffle result log message

--- client/src/main/java/org/apache/uniffle/client/impl/ShuffleWriteClientImpl.java
+++ client/src/main/java/org/apache/uniffle/client/impl/ShuffleWriteClientImpl.java
@@ -837,7 +837,7 @@ public class ShuffleWriteClientImpl implements ShuffleWriteClient {
             getShuffleServerClient(ssi).commitShuffleResult(request);
         if (response.getStatusCode() == StatusCode.SUCCESS) {
           LOG.info(
-              "Committing report shuffle result to "
+              "Committing shuffle result to "
                   + ssi
                   + " for appId["
                   + appId
@@ -849,7 +849,7 @@ public class ShuffleWriteClientImpl implements ShuffleWriteClient {
           }
         } else {
           LOG.warn(
-              "Committing report shuffle result to "
+              "Committing shuffle result to "
                   + ssi
                   + " for appId["
                   + appId
@@ -860,7 +860,7 @@ public class ShuffleWriteClientImpl implements ShuffleWriteClient {
         }
       } catch (Exception e) {
         LOG.warn(
-            "Committing report shuffle result is failed to "
+            "Committing shuffle result is failed to "
                 + ssi
                 + " for appId["
                 + appId
